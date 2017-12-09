0bfa20a4a6d7688321c0ce9b391dcfbafa152dec
Nayan Patil <patilnayan092@yahoo.com>
2017-12-09 05:25:32 +0000

    updated for sample wsgi python file

--- sample_wsgi.py
+++ sample_wsgi.py
@@ -1,12 +1,14 @@
 import os
 import sys
 
-path = '/home/patilnayan092/etonline'
+path = '/home/patilnayan092/etonlinetest'
 if path not in sys.path:
     sys.path.append(path)
 
-os.environ['DJANGO_SETTING_MODULE'] = 'etonline.settings'
+os.environ['DJANGO_SETTING_MODULE'] = 'etonlinetest.settings'
 
 from django.core.wsgi import get_wsgi_application
-from django_contrib.staticfiles.handlers import StaticFilesHandler
-application = StaticFilesHandler(get_wsgi_application())
+application = get_wsgi_application()
+
+import django.core.handlers.wsgi
+application = get_wsgi_application()
